a8888d972bdee6df95d5e5ae39a1e66d38e04ef7
Erik Agsjö <erik.agsjo@gmail.com>
2020-01-07 19:47:56 +0100

    Logging + force bash

--- stamp.sh
+++ stamp.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 package=${1:?Expected package argument}
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
@@ -5,9 +7,11 @@ if output=$(git status --porcelain) && [ -z "$output" ]; then
     echo clean repo >&2
     sha=$(git rev-parse HEAD)
     rev=${sha:0:7}
+    echo rev:$rev >&2
 else
     # dirty
     echo dirty repo >&2
+    echo "$output" >&2
     rev="dev"
 fi
 
